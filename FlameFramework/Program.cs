d8801bca53b1a54a4d0c771ca6a6a3860579c61b
loader <loader@ensage.io>
2017-09-06 21:37:55 +0200

    kappa

---
+++ FlameFramework/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FlameFramework
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("HELLO");
+            await Task.Delay(100);
+            Console.WriteLine("WORLD");
+        }
+    }
+}
